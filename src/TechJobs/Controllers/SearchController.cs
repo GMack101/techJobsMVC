6c982d14ed759af858f2d26ce54b9934d098a94c
Gee <tuca101@cs.com>
2020-01-16 10:44:15 -0600

    techJobsMVC

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -5,16 +5,31 @@ using TechJobs.Models;
 namespace TechJobs.Controllers
 {
     public class SearchController : Controller
+
+    // TODO #1 - Create a Results action method to process 
+    // search request and display results
+
     {
+        public IActionResult Results(string searchType, string searchTerm)
+
+        {
+
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Searchtype, searchterm";
+            ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm); 
+            return View("Index");
+        }  
+
+
+
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
-            ViewBag.title = "Search";
+            ViewBag.title = "Searchtype, searchterm";
             return View();
         }
 
-        // TODO #1 - Create a Results action method to process 
-        // search request and display results
+        
 
     }
 }
